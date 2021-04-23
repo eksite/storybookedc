96a0f3eb0deef5006180a183dce196b68d418106
eksite <denisandipod@gmail.com>
2021-04-24 00:48:09 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+
+const App = () => {
+  return (
+    <div>initial</div>
+  )
+}
+
+export default App;
